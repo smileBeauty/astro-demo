397079ae3d49c97659f692620f581966f65ab187
smileBeauty <995401608@qq.com>
2023-12-18 00:34:18 +0800

    添加rss 动态渲染页面 水合react客户端渲染组件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://657f18f1201fdd04e468266e--mari-astro-demo.netlify.app/",
+  integrations: [react()]
+});
